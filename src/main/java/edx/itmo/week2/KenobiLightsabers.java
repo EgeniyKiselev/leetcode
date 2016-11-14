86fbc2928f380d8c876e8fec1df9c7f3b2898eb8
Evgeny Kiselev <ekiselev@axiomsl.com>
2016-11-14 16:27:06 -0500

    with custom double list implementation

--- src/main/java/edx/itmo/week2/KenobiLightsabers.java
+++ src/main/java/edx/itmo/week2/KenobiLightsabers.java
@@ -85,29 +85,123 @@ public class KenobiLightsabers {
         try (PrintWriter out = newOutput(TASK_NAME)) {
             FastScanner in = newInput(TASK_NAME);
             int N = in.nextInt();
-            Deque<Integer> deq = new LinkedList<>();
+//            Deque<Integer> deq = new LinkedList<>();
+            Saber head = null;//new Saber(null, null, 0);
+            Saber middle = null;
+            Saber tail = null;//new Saber(null, null, 0);
+            int len = 0;
             for (int i = 0; i < N; i++) {
                 String str = in.next();
                 if ("take".equals(str)) {
-                    deq.removeLast();
-                } else if ("mum!".equals(str)) {
-                    int size = deq.size();
-                    for (int j = 0; j < size / 2; j++) {
-                        int t = deq.removeFirst();
-                        deq.addLast(t);
+//                    deq.removeLast();
+                    if (len % 2 == 0) {
+                        middle = middle.prev;
+                        if (middle == null) {
+                            middle = head;
+                        }
                     }
+
+                    tail = removeLast(tail);
+                    if (tail == null) {
+                        head = null;
+                        middle = null;
+                    }
+                    len--;
+                } else if ("mum!".equals(str)) {
+//                    int size = deq.size();
+//                    for (int j = 0; j < size / 2; j++) {
+//                        int t = deq.removeFirst();
+//                        deq.addLast(t);
+//                    }
+
+                    if (len == 0) {
+                        continue;
+                    }
+                    Saber tmpTail = tail;
+
+                    tail.next = head;
+                    tail.next.prev = tail;
+                    tail = middle;
+
+                    head = middle.next;
+                    head.prev = null;
+                    middle = tmpTail;
+                    if (len % 2 != 0) {
+                        middle = middle.prev;
+                    }
+                    tail.next = null;
                 } else {
                     int num = in.nextInt();
-                    deq.addLast(num);
+//                    deq.addLast(num);
+                    if (tail == null) {
+                        Saber s = new Saber(null, null, num);
+                        tail = s;
+                        head = s;
+                        middle = head;
+                    } else {
+                        tail = addLast(tail, num);
+                        if (len == 0) {
+                            middle = head;
+                        } else if (len == 1) {
+                            middle = head;
+                        } else if (len == 2) {
+                            middle = head;
+                        } else if (len % 2 != 0) {
+                            middle = middle.next;
+                        }
+                    }
+                    len++;
                 }
 
             }
-            int size = deq.size();
-            out.println(size);
-            for (int i = 0; i < size; i++) {
-                out.print(deq.removeFirst());
+//            int size = deq.size();
+//            out.println(size);
+//            for (int i = 0; i < size; i++) {
+//                out.print(deq.removeFirst());
+//                out.print(' ');
+//            }
+//            out.println(" ");
+
+            out.println(len);
+            Saber cur = head;
+            while (cur != null) {
+                out.print(cur.number);
                 out.print(' ');
+                cur = cur.next;
             }
+        }
+    }
+    static Saber addLast(Saber s, int n) {
+        if (s != null) {
+            Saber newTail = new Saber(s, null, n);
+            s.next = newTail;
+            newTail.prev = s;
+            return newTail;
+        }
+        return new Saber(null, null, n);
+    }
+
+    static Saber removeLast(Saber s) {
+        if (s != null) {
+            s = s.prev;
+            if (s == null) {
+                return s;
+            }
+            s.next = null;
+            return s;
+        }
+        return null;
+    }
+
+    static class Saber {
+        Saber prev;
+        Saber next;
+        int number;
+
+        public Saber(Saber prev, Saber next, int number) {
+            this.prev = prev;
+            this.next = next;
+            this.number = number;
         }
     }
 }
